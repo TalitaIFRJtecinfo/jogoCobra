ba88405616d0e0982e76719447dab3aef93654c6
𓋹Talita Akame𓋹 <talitacorreacostarj@gmail.com>
2025-12-10 10:12:14 -0300

    Update tela.js

--- js/tela.js
+++ js/tela.js
@@ -1,10 +1,11 @@
 const tela = {
     largura: 360,
     altura: 600,
-    cor: "#85d656ff",
+    cor: "#964b00",
     desenhar(){
         ctx.fillStyle = this.cor;
         ctx.fillRect(0,30,this.largura,this.altura)
     }
 }
+
 
